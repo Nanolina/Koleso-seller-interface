b1c55e2fb1f6a0b86fca4c5df0cc65527671a20a
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-02-05 21:51:04 +0400

    refactor: improve the code for store details form

--- src/modules/stores/components/StoreDetailsForm/StoreDetailsForm.tsx
+++ src/modules/stores/components/StoreDetailsForm/StoreDetailsForm.tsx
@@ -1,5 +1,6 @@
+import { unwrapResult } from '@reduxjs/toolkit';
 import { Field, Form, Formik } from 'formik';
-import { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -18,140 +19,157 @@ import {
   initialValuesStore,
   validationSchemaStore,
 } from '../../storeFormModel';
-import { ICreateStoreData, IStoreDetailsFormProps } from '../../types';
+import { ICreateStoreData, IStore, IStoreDetailsFormProps } from '../../types';
 import { Logo } from '../Logo/Logo';
 import styles from './StoreDetailsForm.module.css';
 
-export const StoreDetailsForm: React.FC<IStoreDetailsFormProps> = ({
-  modalOpen,
-  handleCloseModal,
-}) => {
-  const { t } = useTranslation();
-  const dispatch = useDispatch<AppDispatch>();
-  const { storeId } = useParams<{ storeId: string }>();
+export const StoreDetailsForm: React.FC<IStoreDetailsFormProps> = React.memo(
+  ({ modalOpen, handleCloseModal }) => {
+    const { t } = useTranslation();
+    const dispatch = useDispatch<AppDispatch>();
+    const { storeId } = useParams<{ storeId: string }>();
 
-  // useState
-  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  const [initialValues, setInitialValues] =
-    useState<ICreateStoreData>(initialValuesStore);
+    // useState
+    const [isStoreFound, setIsStoreFound] = useState<boolean>(true);
+    const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+    const [initialValues, setInitialValues] =
+      useState<ICreateStoreData>(initialValuesStore);
 
-  // Values from Redux
-  const { store, items, loading, error, success } = useSelector(
-    (state: IRootState) => state.stores
-  );
-
-  // useEffect
-  useEffect(() => {
-    if (storeId && storeId !== 'new') {
-      dispatch(handleGetStoreById(storeId));
-    }
-  }, [dispatch, storeId]);
-
-  useEffect(() => {
-    if (storeId && storeId !== 'new') {
-      if (store) {
-        setInitialValues({
-          name: store.name,
-          description: store.description || '',
-          logo: store.logo,
-        });
-
-        if (store.logo) setPreviewUrl(store.logo);
-      }
-    }
-  }, [dispatch, storeId, items, store]);
-
-  useEffect(() => {
-    return () => {
-      if (previewUrl) {
-        URL.revokeObjectURL(previewUrl);
-      }
-    };
-  }, [previewUrl]);
-
-  // Early return
-  if (!store && storeId !== 'new') {
-    return (
-      <div className={styles.notFound}>{t('stores.storeDetails.notFound')}</div>
-    );
-  }
-
-  if (loading)
-    return (
-      <Modal isOpen={modalOpen} onClose={handleCloseModal}>
-        <Loader />
-      </Modal>
+    // Values from Redux
+    const { store, loading, error, success } = useSelector(
+      (state: IRootState) => state.stores
     );
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={() => validationSchemaStore(t)}
-      onSubmit={(values) =>
+    // useEffect
+    useEffect(() => {
+      const fetchData = async () => {
+        // If the correct storeId in the url
+        if (storeId && storeId !== 'new') {
+          // Get data of store from DB
+          const data = await dispatch(handleGetStoreById(storeId));
+
+          // Retrieve data from a completed promise
+          const store: IStore = unwrapResult(data);
+
+          // Set initial values based on the data from DB
+          if (store) {
+            setInitialValues({
+              name: store.name,
+              description: store.description || '',
+              logo: store.logo,
+            });
+
+            if (store.logo) setPreviewUrl(store.logo);
+          } else {
+            setIsStoreFound(false);
+          }
+        }
+      };
+
+      fetchData();
+    }, [dispatch, storeId]);
+
+    // Clearing preview logo URL to free up resources
+    useEffect(() => {
+      return () => {
+        if (previewUrl) {
+          URL.revokeObjectURL(previewUrl);
+        }
+      };
+    }, [previewUrl]);
+
+    // Submit data
+    const handleSubmit = useCallback(
+      (values: ICreateStoreData) => {
         handleSubmitFormStore(
           store,
           storeId,
           dispatch,
           setInitialValues,
           values
-        )
-      }
-      enableReinitialize
-    >
-      {({ errors, touched, setFieldValue, isValid, dirty }) => (
-        <Form className={styles.container}>
-          <InputLabel
-            label={t('stores.table.name')}
-            id="name"
-            name="name"
-            errors={errors}
-            touched={touched}
-            required
-          />
-          <Field
-            as={TextareaLabel}
-            label={t('stores.table.description')}
-            id="description"
-            name="description"
-            errors={errors}
-            touched={touched}
-            rows={4}
-          />
+        );
+      },
+      [store, storeId, dispatch]
+    );
 
-          <Logo
-            setFieldValue={setFieldValue}
-            previewUrl={previewUrl}
-            setPreviewUrl={setPreviewUrl}
-          />
+    // Early returns
+    if (!isStoreFound) {
+      return (
+        <div className={styles.notFound}>
+          {t('stores.storeDetails.notFound')}
+        </div>
+      );
+    }
 
-          <div className={styles.buttonContainer}>
-            <Button
-              text={t('save')}
-              type="submit"
-              disabled={!isValid || !dirty}
+    if (loading)
+      return (
+        <Modal isOpen={modalOpen} onClose={handleCloseModal}>
+          <Loader />
+        </Modal>
+      );
+
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={() => validationSchemaStore(t)}
+        onSubmit={handleSubmit}
+        enableReinitialize
+      >
+        {({ errors, touched, setFieldValue, isValid, dirty }) => (
+          <Form className={styles.container}>
+            <InputLabel
+              label={t('stores.table.name')}
+              id="name"
+              name="name"
+              errors={errors}
+              touched={touched}
+              required
+            />
+            <Field
+              as={TextareaLabel}
+              label={t('stores.table.description')}
+              id="description"
+              name="description"
+              errors={errors}
+              touched={touched}
+              rows={4}
             />
 
-            {storeId && (
-              <span
-                className="removeText"
-                onClick={() =>
-                  handleRemoveFormStore(
-                    storeId,
-                    dispatch,
-                    previewUrl,
-                    setPreviewUrl
-                  )
-                }
-              >
-                {t('stores.storeDetails.removeStore')}
-              </span>
-            )}
-          </div>
+            <Logo
+              setFieldValue={setFieldValue}
+              previewUrl={previewUrl}
+              setPreviewUrl={setPreviewUrl}
+            />
 
-          {error && <MessageBox errorMessage={error} />}
-          {success && <MessageBox successMessage={success} />}
-        </Form>
-      )}
-    </Formik>
-  );
-};
+            <div className={styles.buttonContainer}>
+              <Button
+                text={t('save')}
+                type="submit"
+                disabled={!isValid || !dirty}
+              />
+
+              {storeId && (
+                <span
+                  className="removeText"
+                  onClick={() =>
+                    handleRemoveFormStore(
+                      storeId,
+                      dispatch,
+                      previewUrl,
+                      setPreviewUrl
+                    )
+                  }
+                >
+                  {t('stores.storeDetails.removeStore')}
+                </span>
+              )}
+            </div>
+
+            {error && <MessageBox errorMessage={error} />}
+            {success && <MessageBox successMessage={success} />}
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+);
